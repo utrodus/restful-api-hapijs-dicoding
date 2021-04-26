0f42953d03935488c70409aae035950b91d31b89
utrodus <come.programmer@gmail.com>
2021-04-26 08:33:28 +0700

    Implements method / verb request & routes

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const Hapi = require('@hapi/hapi');
-
-const init = async ()=>{
+const routes = require('./routes');
+const init = async () => {
     const server = Hapi.server({
-        port:5000,
-        host:'localhost'
+        port: 5000,
+        host: 'localhost'
     });
+
+    server.route(routes);
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
